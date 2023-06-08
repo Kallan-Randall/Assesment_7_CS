6842598be87d441b3ceca159aadf10c443590015
unknown <kallan92@gmail.com>
2023-06-08 15:43:22 -0600

    finished note file

--- notes.js
+++ notes.js
@@ -21,3 +21,9 @@
 
 // insert 1.2798733 s
 // append 3.5314 ms
+
+
+// It appears that the smaller the array, the faster the insert function is. 
+// However as the array becomes larger the insert function begins to take longer and longer. 
+// The inverse happens when we look at the appeand function. The append takes longer with the smaller arrays
+// but then becomes much faster as the arrays grow in size.
