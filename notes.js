0d2a812ba8f20425f8ca0ae5537ee3f3f881b114
unknown <kallan92@gmail.com>
2023-06-08 16:48:00 -0600

    added some notes in the nots file

--- notes.js
+++ notes.js
@@ -27,3 +27,7 @@
 // However as the array becomes larger the insert function begins to take longer and longer. 
 // The inverse happens when we look at the appeand function. The append takes longer with the smaller arrays
 // but then becomes much faster as the arrays grow in size.
+// The insert is faster while the array is small because it is only has to "shift" a few items to make room. As the array
+// grows larger it slows down because it has to shift more items.
+// The append becomes much faster in the end because it is only appending at the end of the array. This makes it much faster
+// as the array grows larger, it is only looking for the end and adding there rather than shifting everything down.
