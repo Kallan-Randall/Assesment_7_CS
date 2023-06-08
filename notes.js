19c6e87676ef02bd764fcee20b3cb997dee7f0aa
unknown <kallan92@gmail.com>
2023-06-08 15:26:23 -0600

    initial commit

---
+++ notes.js
@@ -0,0 +1,23 @@
+// tinyArray results
+
+// insert 46.1 μs
+// append 118 μs
+
+// smallArray results
+
+// insert 67.9 μs
+// append 132.5 μs
+
+// mediumArray results
+// insert 221 μs
+// append 181 μs
+
+// largeArray
+
+// insert 11.2819 ms
+// append 663.2 μs
+
+// Extralargearray results
+
+// insert 1.2798733 s
+// append 3.5314 ms
